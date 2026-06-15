0a299994d165e6918871eec4d58d89e87a481de9
Roei <Roeit1977@gmail.com>
2026-06-15 11:47:02 +0300

    template communities, color means role in a community

--- javascript/main.js
+++ javascript/main.js
@@ -165,11 +165,118 @@ const chats = [{
     createdAt: 1767564832000
 }]
 
-const communities = [{
-    _id: "1", communityName: "meowers", communityLogo: "",
-    communityBakcground: "", ownerId: "", moderators: [],
-    members: ["4"], posts: ["2", "5"], createdAt: 1767564832000
-}]
+const communities = [
+  {
+    _id: "1",
+    communityName: "Meower Haven",
+    communityLogo: "",
+    communityBakcground: "",
+    ownerId: "1",
+    moderators: ["2"],
+    members: ["1", "2", "4"],
+    posts: ["1", "3"],
+    createdAt: Date.now() - 1000 * 60 * 60 * 24 * 10
+  },
+  {
+    _id: "2",
+    communityName: "Purrfect Coders",
+    communityLogo: "",
+    communityBakcground: "",
+    ownerId: "4",
+    moderators: ["1", "3"],
+    members: ["4", "1", "3"],
+    posts: ["2", "4", "5"],
+    createdAt: Date.now() - 1000 * 60 * 60 * 24 * 200
+  },
+  {
+    _id: "3",
+    communityName: "Whisker Warriors",
+    communityLogo: "",
+    communityBakcground: "",
+    ownerId: "2",
+    moderators: [],
+    members: ["2", "3"],
+    posts: ["6"],
+    createdAt: Date.now() - 1000 * 60 * 60 * 24 * 400
+  },
+  {
+    _id: "4",
+    communityName: "Claw & Order",
+    communityLogo: "",
+    communityBakcground: "",
+    ownerId: "3",
+    moderators: ["4"],
+    members: ["3", "4"],
+    posts: ["7", "8"],
+    createdAt: Date.now() - 1000 * 60 * 60 * 24 * 600
+  },
+  {
+    _id: "5",
+    communityName: "Catnip Collective",
+    communityLogo: "",
+    communityBakcground: "",
+    ownerId: "1",
+    moderators: [],
+    members: ["1", "4"],
+    posts: ["9"],
+    createdAt: Date.now() - 1000 * 60 * 60 * 24 * 800
+  },
+  {
+    _id: "6",
+    communityName: "Feline Fitness Club",
+    communityLogo: "",
+    communityBakcground: "",
+    ownerId: "4",
+    moderators: [],
+    members: ["4"],
+    posts: ["10"],
+    createdAt: Date.now() - 1000 * 60 * 60 * 24 * 1000
+  },
+  {
+    _id: "7",
+    communityName: "Memeow Cinema",
+    communityLogo: "",
+    communityBakcground: "",
+    ownerId: "2",
+    moderators: ["4"],
+    members: ["2", "4"],
+    posts: ["1", "5"],
+    createdAt: Date.now() - 1000 * 60 * 60 * 24 * 1100
+  },
+  {
+    _id: "8",
+    communityName: "Startup Cats",
+    communityLogo: "",
+    communityBakcground: "",
+    ownerId: "3",
+    moderators: ["1"],
+    members: ["3", "1"],
+    posts: ["2", "6"],
+    createdAt: Date.now() - 1000 * 60 * 60 * 24 * 1200
+  },
+  {
+    _id: "9",
+    communityName: "Book of Paws",
+    communityLogo: "",
+    communityBakcground: "",
+    ownerId: "1",
+    moderators: ["2"],
+    members: ["1", "2"],
+    posts: ["3", "7"],
+    createdAt: Date.now() - 1000 * 60 * 60 * 24 * 1500
+  },
+  {
+    _id: "10",
+    communityName: "Random Meowments",
+    communityLogo: "",
+    communityBakcground: "",
+    ownerId: "2",
+    moderators: [],
+    members: ["2", "3", "4"],
+    posts: ["4", "8", "9"],
+    createdAt: Date.now() - 1000 * 60 * 60 * 24 * 900
+  }
+]
 
 let postsForRender = posts.filter(p => true)
 
